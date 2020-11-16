7cc328d24c2e34474770cc95f48a93857ce1240b
buscaroli <m.buscaroli@outlook.com>
2020-11-16 19:28:34 +0000

    Now also render images in the posts. Need to figure out how to render an image gallery.

--- gatsby-config.js
+++ gatsby-config.js
@@ -19,6 +19,22 @@ module.exports = {
         name: 'src',
         path: `${__dirname}/src/`
       }
+    },
+    'gatsby-plugin-sharp',
+    {
+      resolve: 'gatsby-transformer-remark',
+      options: {
+        plugins: [
+          'gatsby-remark-relative-images',
+          {
+            resolve: 'gatsby-remark-images',
+            options: {
+              maxWidth: 150,
+              linkImagesToOriginal: false
+            }
+          }
+        ]
+      }
     }
   ],
 }
---
+++ src/posts/third.md
@@ -0,0 +1,6 @@
+---
+title: "image post"
+author: "froggy"
+date: "2020-11-16"
+---
+![Picture of a little frog](./frog.png)
